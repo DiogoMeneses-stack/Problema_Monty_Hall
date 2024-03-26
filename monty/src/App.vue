6c20a71763581a887dcbc71b8bc0cd6d97bb8499
DiogoMeneses-stack <henriquediogo434@gmail.com>
2024-03-26 15:51:11 -0300

    Initial Commit.

---
+++ monty/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Problema de Monty hall</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+
+</script>
+
+<style>
+
+*{
+  font-family: 'Montserrat', sans-serif;
+  box-sizing: border-box;
+}
+
+body{
+  color: white;
+  background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+}
+
+#app{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h1{
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+
+</style>
