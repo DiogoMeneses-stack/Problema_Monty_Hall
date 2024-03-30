5fad9725e8b8ec943d108ea0d688331d9dd3204d
DiogoMeneses-stack <henriquediogo434@gmail.com>
2024-03-30 12:44:32 -0300

    Implements the door.

--- monty/src/App.vue
+++ monty/src/App.vue
@@ -1,42 +1,41 @@
 <template>
   <div id="app">
-    <h1>Problema de Monty hall</h1>
-    <GiftDisplay />
+      <h1>Problema de Montyhall</h1>
+      <Door number="1" :hasGift="false" />
   </div>
 </template>
-
+ 
 <script>
-import GiftDisplay from './components/Gift.vue'
-
+import Door from './components/Door.vue'
+ 
 export default {
-    name:'App',
-    components:{GiftDisplay}
+  name: 'App',
+  components: { Door }
 }
 </script>
-
+ 
 <style>
-
-*{
-  font-family: 'Montserrat', sans-serif;
+* {
   box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
 }
-
-body{
-  color: white;
+ 
+body {
+  color: #fff;
   background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
 }
-
-#app{
+ 
+#app {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
-
-#app h1{
+ 
+#app h1 {
   border: 1px solid #000;
   background-color: #0004;
   padding: 20px;
   margin-bottom: 60px;
 }
-
+ 
 </style>
